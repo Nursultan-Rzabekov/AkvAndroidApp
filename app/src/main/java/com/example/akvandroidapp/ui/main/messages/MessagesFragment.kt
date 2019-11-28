b41ff2d8edb1c499675403703b20269e4cce5437
foodsnow <dut.kht@gmail.com>
2019-11-28 18:40:47 +0600

    map added

--- app/src/main/java/com/example/akvandroidapp/ui/main/messages/MessagesFragment.kt
+++ app/src/main/java/com/example/akvandroidapp/ui/main/messages/MessagesFragment.kt
@@ -4,16 +4,26 @@ package com.example.akvandroidapp.ui.main.messages
 import android.os.Bundle
 import android.view.*
 import com.example.akvandroidapp.R
+import com.yandex.mapkit.MapKitFactory
 
 
 class MessagesFragment : BaseMessagesFragment(){
+
+    private val MAPKIT_API_KEY = "74ae21de-d88e-44d9-9a2f-e02de671f696"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.terms_of_use, container, false)
+
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+        MapKitFactory.initialize(context)
+
+        val layout =  inflater.inflate(R.layout.explore_map, container, false)
+
+
+        return layout
     }
 }
 
