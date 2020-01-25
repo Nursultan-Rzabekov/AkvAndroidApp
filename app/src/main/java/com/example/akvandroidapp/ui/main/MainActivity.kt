f8aa53696633efd89259e906a54ffbcdeb62dbbf
Nursultan Rzabekov <nursbrat991229@gmail.com>
2020-01-25 14:09:54 +0600

    merge changes

--- app/src/main/java/com/example/akvandroidapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/akvandroidapp/ui/main/MainActivity.kt
@@ -202,6 +202,7 @@ class MainActivity : BaseActivity(),
         }
 
 //        sessionManager.login(AuthToken(1,"305f6398506c603c7b811cd48c039d9d5e4687ea"))
+        //qwe
         subscribeObservers()
     }
 
